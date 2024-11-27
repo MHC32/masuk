9475b55e76dcf7c203e259c0acc98ec8a21a2824
MHC32 <hantzmichaelchery6@gmail.com>
2024-11-27 11:52:08 -0500

    structur tamagui is done

---
+++ tamagui.config.ts
@@ -0,0 +1,120 @@
+import {createTamagui, createTokens, createFont} from 'tamagui'
+
+
+const size = {
+  small: 16,
+  medium: 19,
+  large: 22,
+  true: 16,
+}
+
+
+const radius = {
+  small: 4,
+  medium: 8,
+  large: 12,
+  true: 8,
+}
+
+const zIndex = {
+    low: 1,
+    medium: 10,
+    high: 100,
+    true: 1,
+  };
+
+
+const interFont = createFont({
+    family: 'Poppins, sans-serif',
+    size: {
+      1: 12,
+      2: 14,
+      3: 15,
+    },
+    lineHeight: {
+      // 1 will be 22
+      2: 22,
+    },
+    weight: {
+      1: '300',
+      // 2 will be 300
+      3: '600',
+    },
+    letterSpacing: {
+      1: 0,
+      2: -1,
+      // 3 will be -1
+    },
+    // (native only) swaps out fonts by face/style
+    face: {
+      300: { normal: 'InterLight', italic: 'InterItalic' },
+      600: { normal: 'InterBold' },
+    },
+})
+
+export const tokens = createTokens({
+    size,
+    radius,
+    zIndex,
+    space: {small:8, medium:12, large:16, true: 12},
+    color: {
+      primary: '#3498db',
+      secondary: '#2ecc71',
+      tertiary: '#FFFFFF',
+      quaternary: '#000000',
+      quinary: '#736A6A',
+    },
+  })
+  
+  const config = createTamagui({
+    tokens,
+  
+    fonts:{
+      heading: interFont,
+      body: interFont,
+    },
+  
+     themes: {
+        light: {
+          bg: tokens.color.tertiary,
+          color: tokens.color.quaternary,
+        },
+        dark: {
+          bg: tokens.color.quaternary,
+          color: tokens.color.tertiary,
+        },
+      },
+    
+      shorthands: {
+        px: 'paddingHorizontal',
+        f: 'flex',
+        m: 'margin',
+        w: 'width',
+        ai: 'alignItems',
+        as: 'alignSelf',
+        bg: 'backgroundColor',
+        br: 'borderRadius',
+        h: 'height',
+        jc: 'justifyContent',
+        p: 'padding',
+        mt: 'marginTop',
+        fw: 'fontWeight',
+        fs: 'fontSize',
+      } as const,
+  })
+
+
+  type AppConfig = typeof config
+
+
+  declare module 'tamagui'{
+    interface tamaguiCustomConfig extends AppConfig {}
+
+
+    interface typeOverride {
+        groupNames(): 'a' | 'b' | 'c'
+    }
+  }
+
+  export default config
+  
