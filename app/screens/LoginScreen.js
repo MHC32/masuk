748aadbdeb401087a30bf9cdf0057c4a21748a05
MHC32 <hantzmichaelchery6@gmail.com>
2024-11-29 13:30:06 -0500

    text is correct

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,15 +1,21 @@
-import { YStack, Input, XStack } from 'tamagui';
-import { Image, StatusBar, Text, ActivityIndicator } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import React from 'react';
-import { tokens } from '../../tamagui.config';
-import { useFonts } from 'expo-font';
+import { YStack, Input, XStack, Button } from "tamagui";
+import {
+  Image,
+  StatusBar,
+  Text,
+  ActivityIndicator,
+  TouchableOpacity,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import React from "react";
+import { tokens } from "../../tamagui.config";
+import { useFonts } from "expo-font";
 
 const LoginScreen = ({ navigation }) => {
   const [fontsLoaded] = useFonts({
-    'Poppins-Semibold': require('../../assets/fonts/Poppins-SemiBold.ttf'),
-    'Poppins-Regular': require('../../assets/fonts/Poppins-Regular.ttf'),
-  })
+    "Poppins-Semibold": require("../../assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-Regular": require("../../assets/fonts/Poppins-Regular.ttf"),
+  });
   if (!fontsLoaded) {
     return <ActivityIndicator />;
   }
@@ -17,30 +23,84 @@ const LoginScreen = ({ navigation }) => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <StatusBar backgroundColor="#FFFFFF" barStyle="dark-content" />
-      <YStack 
-        gap="5%%" 
-        w="100%" 
-        h="100%" 
+      <YStack
+        gap="5%%"
+        w="100%"
+        h="100%"
         bg={tokens.color.tertiary}
-        ai="center" 
+        ai="center"
       >
-        <Image 
-          source={require('../../assets/Logo.png')} 
-          style={{ width: 249, height: 96, marginTop:'15%'}} 
+        <Image
+          source={require("../../assets/Logo.png")}
+          style={{ width: 249, height: 96, marginTop: "15%" }}
         />
-        <Text style={{fontFamily:'Poppins-Semibold', fontSize:38, letterSpacing:2, marginTop:'15%'}}>Masuk</Text>
-        <YStack w='279' h='80'  marginTop='10%' jc='space-around'>
-          <Text style={{fontSize:14, fontFamily:'Poppins-Regular'}} >Masukan Email</Text>
-          <Input size='68' h='43' placeholder='Email' borderColor={tokens.color.Senary} borderRadius={12}/>
+        <Text
+          style={{
+            fontFamily: "Poppins-Semibold",
+            fontSize: 38,
+            letterSpacing: 2,
+            marginTop: "15%",
+          }}
+        >
+          Masuk
+        </Text>
+        <YStack w="279" h="80" marginTop="10%" jc="space-around">
+          <Text style={{ fontSize: 14, fontFamily: "Poppins-Regular" }}>
+            Masukan Email
+          </Text>
+          <Input
+            size="68"
+            h="43"
+            placeholder="Email"
+            borderColor={tokens.color.Senary}
+            borderRadius={12}
+          />
         </YStack>
-        <YStack w='279' h='80'  marginTop='9%' jc='space-around'>
-          <Text style={{fontSize:14, fontFamily:'Poppins-Regular'}} >Masukan Password</Text>
-          <Input size='68' h='43' placeholder='Password' borderColor={tokens.color.Senary} borderRadius={12}/>
+        <YStack w="279" h="80" marginTop="9%" jc="space-around">
+          <Text style={{ fontSize: 14, fontFamily: "Poppins-Regular" }}>
+            Masukan Password
+          </Text>
+          <Input
+            size="68"
+            h="43"
+            placeholder="Password"
+            borderColor={tokens.color.Senary}
+            borderRadius={12}
+          />
         </YStack>
-        <XStack w='300' jc='space-around' marginTop='10'>
-          <Text>Belum punya akun?</Text>
-          <Text style={{fontFamily:'Poppins-Regular', fontSize:13, color:'#4285F4'}} >Lupa Password?</Text>
+        <XStack w="300" jc="space-around" marginTop="10">
+          <Text style={{ fontFamily: "Poppins-Regular", fontSize: 13 }}>
+            Belum punya akun?
+          </Text>
+          <Text
+            style={{
+              fontFamily: "Poppins-Regular",
+              fontSize: 13,
+              color: "#4285F4",
+            }}
+          >
+            Lupa Password?
+          </Text>
         </XStack>
+        <YStack w="279" h="25%" justifyContent="center" ai="center">
+          <TouchableOpacity
+            style={{
+              height: 55,
+              width: 218,
+              backgroundColor: "#4285F4",
+              borderRadius: 12,
+              justifyContent: "center",
+              alignItems:'center',
+              shadowColor: "#000",
+              shadowOffset: { width: 4, height: 4 },
+              shadowOpacity: 0.16,
+              shadowRadius: 8,
+              elevation: 8,
+            }}
+          >
+            {/* <Text>Masuk</Text> */}
+          </TouchableOpacity>
+        </YStack>
       </YStack>
     </SafeAreaView>
   );
