ab22831a2f45c4b68aa0a8b32431836424a6cf6a
MHC32 <hantzmichaelchery6@gmail.com>
2024-12-01 22:26:16 -0500

    reflexion done comming correction and other page

--- tamagui.config.ts
+++ tamagui.config.ts
@@ -1,4 +1,4 @@
-import {createTamagui, createTokens, createFont} from 'tamagui'
+import { createTamagui, createTokens, createFont } from 'tamagui'
 const { widthPercentageToDP: wp, heightPercentageToDP: hp } = require('react-native-responsive-screen');
 
 const size = {
@@ -6,9 +6,9 @@ const size = {
   medium: 19,
   large: 22,
   true: 16,
-  responsiveContainerWidth: wp('100%'), 
+  responsiveContainerWidth: wp('100%'),
   responsiveContainerHeight: hp('100%'),
- 
+
 }
 
 
@@ -20,106 +20,105 @@ const radius = {
 }
 
 const zIndex = {
-    low: 1,
-    medium: 10,
-    high: 100,
-    true: 1,
-  };
+  low: 1,
+  medium: 10,
+  high: 100,
+  true: 1,
+};
 
 
 const interFont = createFont({
-    family: 'Poppins, sans-serif',
-    size: {
-      1: 12,
-      2: 14,
-      3: 15,
-    },
-    lineHeight: {
-      // 1 will be 22
-      2: 22,
-    },
-    weight: {
-      1: '300',
-      // 2 will be 300
-      3: '600',
-    },
-    letterSpacing: {
-      1: 0,
-      2: -1,
-      // 3 will be -1
-    },
-    // (native only) swaps out fonts by face/style
-    face: {
-      300: { normal: 'InterLight', italic: 'InterItalic' },
-      600: { normal: 'InterBold' },
-    },
+  family: 'Poppins, sans-serif',
+  size: {
+    1: 12,
+    2: 14,
+    3: 15,
+  },
+  lineHeight: {
+    // 1 will be 22
+    2: 22,
+  },
+  weight: {
+    1: '300',
+    // 2 will be 300
+    3: '600',
+  },
+  letterSpacing: {
+    1: 0,
+    2: -1,
+    // 3 will be -1
+  },
+  // (native only) swaps out fonts by face/style
+  face: {
+    300: { normal: 'InterLight', italic: 'InterItalic' },
+    600: { normal: 'InterBold' },
+  },
 })
 
 export const tokens = createTokens({
-    size,
-    radius,
-    zIndex,
-    space: {small:8, medium:12, large:16, true: 12},
-    color: {
-      primary: '#3498db',
-      secondary: '#2ecc71',
-      tertiary: '#FFFFFF',
-      quaternary: '#000000',
-      quinary: '#736A6A',
-      Senary: '#4285F4'
+  size,
+  radius,
+  zIndex,
+  space: { small: 8, medium: 12, large: 16, true: 12 },
+  color: {
+    primary: '#3498db',
+    secondary: '#2ecc71',
+    tertiary: '#FFFFFF',
+    quaternary: '#000000',
+    quinary: '#736A6A',
+    Senary: '#4285F4'
 
+  },
+})
+
+const config = createTamagui({
+  tokens,
+
+  fonts: {
+    heading: interFont,
+    body: interFont,
+  },
+
+  themes: {
+    light: {
+      bg: tokens.color.tertiary,
+      color: tokens.color.quaternary,
     },
-  })
-  
-  const config = createTamagui({
-    tokens,
-  
-    fonts:{
-      heading: interFont,
-      body: interFont,
+    dark: {
+      bg: tokens.color.quaternary,
+      color: tokens.color.tertiary,
     },
-  
-     themes: {
-        light: {
-          bg: tokens.color.tertiary,
-          color: tokens.color.quaternary,
-        },
-        dark: {
-          bg: tokens.color.quaternary,
-          color: tokens.color.tertiary,
-        },
-      },
-    
-      shorthands: {
-        px: 'paddingHorizontal',
-        f: 'flex',
-        m: 'margin',
-        w: 'width',
-        ai: 'alignItems',
-        as: 'alignSelf',
-        bg: 'backgroundColor',
-        br: 'borderRadius',
-        h: 'height',
-        jc: 'justifyContent',
-        p: 'padding',
-        mt: 'marginTop',
-        fw: 'fontWeight',
-        fs: 'fontSize',
-      } as const,
-  })
+  },
+
+  shorthands: {
+    px: 'paddingHorizontal',
+    f: 'flex',
+    m: 'margin',
+    w: 'width',
+    ai: 'alignItems',
+    as: 'alignSelf',
+    bg: 'backgroundColor',
+    br: 'borderRadius',
+    h: 'height',
+    jc: 'justifyContent',
+    p: 'padding',
+    mt: 'marginTop',
+    fw: 'fontWeight',
+    fs: 'fontSize',
+  } as const,
+})
 
 
-  type AppConfig = typeof config
+type AppConfig = typeof config
 
 
-  declare module 'tamagui'{
-    interface tamaguiCustomConfig extends AppConfig {}
+declare module 'tamagui' {
+  interface tamaguiCustomConfig extends AppConfig { }
 
 
-    interface typeOverride {
-        groupNames(): 'a' | 'b' | 'c'
-    }
+  interface typeOverride {
+    groupNames(): 'a' | 'b' | 'c'
   }
+}
 
-  export default config
-  
+export default config
